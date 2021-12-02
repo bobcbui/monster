4b9aebcd75da249d5a2a565a8dcaca278c0be4da
one-few <792190997@qq.com>
2021-12-02 20:25:14 +0800

    提交成吨的代码

---
+++ web-chat/src/views/group_list.vue
@@ -0,0 +1,80 @@
+<template>
+  <div
+    style="
+      height: calc(100% - 21px);
+      margin: auto;
+      width: 100%;
+      max-width: 800px;
+    "
+  >
+    <div
+      style="
+        height: calc(100%);
+        width: 100%;
+        overflow-y: scroll;
+        border: 1px solid black;
+        border-bottom: 0px;
+      "
+    >
+      <p
+        v-for="(item, index) in groupList"
+        :key="index"
+        style="padding: 5px"
+        @click="join(item.id)"
+      >
+        {{ item.name }} <button style="float:right">加入</button>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+import request from "../utils/request";
+export default {
+  name: "group_list",
+  data() {
+    return {
+      groupList: [],
+    };
+  },
+  computed: {},
+  created() {},
+  methods: {
+    loadGangList() {
+      request({
+        url: "/gang/list",
+        method: "get",
+      }).then((response) => {
+        this.groupList = response.data;
+      });
+    },
+    join(id) {
+      request({
+        url: "/gang/insert",
+        method: "post",
+        data:{id:id}
+      }).then((response) => {
+        alert(jrcg)
+      });
+    },
+  },
+  mounted() {
+    this.loadGangList();
+  },
+};
+</script>
+
+<style scoped>
+.send-but {
+  position: fixed;
+  bottom: 0px;
+}
+.message-body {
+  height: 500px;
+  width: 300px;
+  background: rgb(167, 166, 166);
+}
+.message {
+  padding-bottom: 10px;
+}
+</style>
---
+++ web-chat/src/views/setting.vue
@@ -0,0 +1,60 @@
+<template>
+  <div style="height: calc(100% - 21px);  margin: auto;width: 100%;max-width: 800px;">
+    <div style="height: 100%;width:100%; overflow-y: scroll;padding:5px; border:1px solid black;border-bottom:0px">
+      <p>用户名：ask的JFK</p>
+      <p>简介：askdsfasdfasdfasdfasdfasdf的JFK</p>
+      <p>性别：女（暂时无法修改性别）</p>
+      <p>是否可以私信：是 , 否</p>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "home",
+  data() {
+    return {
+      groupId:this.$route.params.groupId,
+      formData:{
+        to:this.$route.params.groupId,
+        messageText:"",
+        type:"group"
+      }
+    };
+  },
+  computed: {
+    
+  },
+  created() {
+
+  },
+  methods: {
+    toMessage(id){
+      this.$router.push({
+        path: '/group/'+ id
+    })
+    }
+  },
+  mounted(){
+   // alert(JSON.stringify(this.$store.state.message))
+  }
+};
+</script>
+
+<style scoped>
+.send-but{
+  position:fixed;
+  bottom: 0px;
+}
+.message-body{
+  height: 500px;
+  width: 300px;
+  background: rgb(167, 166, 166);
+}
+.message{
+  padding-bottom: 10px;
+}
+p{
+    padding: 10px;
+}
+
+</style>
